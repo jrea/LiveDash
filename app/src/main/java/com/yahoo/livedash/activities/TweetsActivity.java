108b6448f494be62c27fc80941e887741bfed264
Joseph Rea <josephr@yahoo-inc.com>
2014-10-09 18:17:55 -0700

    fix menu on placeholder activities

--- app/src/main/java/com/yahoo/livedash/activities/TweetsActivity.java
+++ app/src/main/java/com/yahoo/livedash/activities/TweetsActivity.java
@@ -1,6 +1,7 @@
 package com.yahoo.livedash.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,10 +28,16 @@ public class TweetsActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void goToProfile(MenuItem item) {
+        Intent i = new Intent(this, ProfileActivity.class);
+        startActivity(i);
+    }
+
+    public void goToEvents(MenuItem item) {
+        Intent i = new Intent(this, EventActivity.class);
+        startActivity(i);
     }
 }
